10790b91412a4d10d4dce18cee1c989ca2a1150f
tygras <tygras.mos@gmail.com>
2022-06-03 10:03:34 +0300

    Home Work 2: Refactoring.

--- home-work-2/src/main/java/ru/otus/spring/model/AnswersToTestQuestionsModel.java
+++ home-work-2/src/main/java/ru/otus/spring/model/AnswersToTestQuestionsModel.java
@@ -3,12 +3,28 @@ package ru.otus.spring.model;
 
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@Data
+
 public class AnswersToTestQuestionsModel {
 
     private String fullName;
     private List<AnswerOnQuestionModel> questionResultModelList;
 
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public List<AnswerOnQuestionModel> getQuestionResultModelList() {
+        return questionResultModelList;
+    }
+
+    public void setQuestionResultModelList(List<AnswerOnQuestionModel> questionResultModelList) {
+        this.questionResultModelList = new ArrayList<>(questionResultModelList);
+    }
 }
