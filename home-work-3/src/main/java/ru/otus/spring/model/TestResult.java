2f0d7c72dc53ecf7e2a9fdcc58afb6fff5c17bab
tygras <tygras.mos@gmail.com>
2022-07-08 09:51:49 +0300

    HW-3. Immutability added to all models.

--- home-work-3/src/main/java/ru/otus/spring/model/TestResult.java
+++ home-work-3/src/main/java/ru/otus/spring/model/TestResult.java
@@ -1,8 +1,8 @@
 package ru.otus.spring.model;
 
-import lombok.Data;
 
-@Data
+import java.util.ArrayList;
+
 public class TestResult {
 
     private AnswersToTestQuestionsModel answersToTestQuestionsModel;
@@ -11,4 +11,47 @@ public class TestResult {
     private Integer testScore;
     private Boolean isOffset;
 
+    public AnswersToTestQuestionsModel getAnswersToTestQuestionsModel() {
+        return answersToTestQuestionsModel;
+    }
+
+    public void setAnswersToTestQuestionsModel(AnswersToTestQuestionsModel answersToTestQuestionsModel) {
+        AnswersToTestQuestionsModel newAnswersToTestQuestionsModel = new AnswersToTestQuestionsModel();
+        newAnswersToTestQuestionsModel.setFullName(answersToTestQuestionsModel.getFullName());
+        newAnswersToTestQuestionsModel.setQuestionResultModelList(
+                new ArrayList<>(answersToTestQuestionsModel.getQuestionResultModelList()));
+        this.answersToTestQuestionsModel = newAnswersToTestQuestionsModel;
+    }
+
+    public Integer getQuestionsQuantity() {
+        return questionsQuantity;
+    }
+
+    public void setQuestionsQuantity(Integer questionsQuantity) {
+        this.questionsQuantity = questionsQuantity;
+    }
+
+    public Integer getCorrectAnswersQuantity() {
+        return correctAnswersQuantity;
+    }
+
+    public void setCorrectAnswersQuantity(Integer correctAnswersQuantity) {
+        this.correctAnswersQuantity = correctAnswersQuantity;
+    }
+
+    public Integer getTestScore() {
+        return testScore;
+    }
+
+    public void setTestScore(Integer testScore) {
+        this.testScore = testScore;
+    }
+
+    public Boolean getOffset() {
+        return isOffset;
+    }
+
+    public void setOffset(Boolean offset) {
+        isOffset = offset;
+    }
 }
